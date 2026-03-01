996a3ef61e5d743eca35a1f3611b4ffb8d67d577
B3Z0 <vlad.cotiga75@egmail.com>
2026-03-01 16:42:29 +0100

    map import

---
+++ src/game.rs
@@ -0,0 +1,31 @@
+use macroquad::prelude::*;
+use macroquad_tiled_clone::Map as TiledMap;
+use std::path::Path;
+
+pub async fn run() {
+    let game = Game::new().await;
+
+    loop {
+        game.frame().await;
+    }
+}
+
+struct Game {
+    tiled_map: TiledMap,
+}
+
+impl Game {
+    async fn new() -> Self {
+        let tiled_map = TiledMap::load("game/map.json")
+            .await
+            .expect("Failed to load map");
+        Self { tiled_map }
+    }
+
+    async fn frame(&mut self) {
+        clear_background(WHITE);
+        let view_min = vec2(0.0, 0.0);
+        let view_max = vec2(screen_width(), screen_height());
+        self.tiled_map.draw(view_min, view_max);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::Conf;
+
+fn window_conf() -> Conf {
+    let mut conf = Conf {
+        window_title: "Chat RPG".to_owned(),
+        ..Default::default()
+    };
+
+    if let Ok(map) = rpg::map::RuntimeMapAdapter::from_tiled_json_wall_layer("game/map.json") {
+        conf.window_width = (map.width as f32 * map.tile_size).round() as i32;
+        conf.window_height = (map.height as f32 * map.tile_size).round() as i32;
+    }
+
+    conf
+}
+
+#[macroquad::main("RPG Chat Sandbox")]
+async fn main() {
+    rpg::game::run().await;
 }
